3919bd415790f24c13f7274691b091985ec80c2d
oriolorra <oriolorraserra@gmail.com>
2015-12-01 20:05:26 +0100

    first commit example 1

---
+++ eigen_example1/build/CMakeFiles/eigenExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eigenExample.dir/eigenExample.cpp.o"
+  "eigenExample.pdb"
+  "eigenExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eigenExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
