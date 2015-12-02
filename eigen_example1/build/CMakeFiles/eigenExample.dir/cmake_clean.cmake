b134c877e26e259f290bcb0b41c50276ac05ce30
oriolorra <oriolorraserra@gmail.com>
2015-12-02 09:05:56 +0100

    optimized code

---
+++ eigen_example1/build/CMakeFiles/eigenExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eigenExample.dir/src/eigenExample.cpp.o"
+  "eigenExample.pdb"
+  "eigenExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eigenExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
